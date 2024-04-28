3539a3b4fe6aaa805539dd5376c64558600bbbe8
david <davidqhr@gmail.com>
2024-04-28 00:51:01 -0700

    update

--- src/color-thief-node.js
+++ src/color-thief-node.js
@@ -1,5 +1,5 @@
-const getPixels = require('get-pixels');
-const quantize = require('@lokesh.dhakar/quantize');
+const getPixels = require("get-pixels");
+const quantize = require("@lokesh.dhakar/quantize");
 
 function createPixelArray(imgData, pixelCount, quality) {
     const pixels = imgData;
@@ -13,7 +13,7 @@ function createPixelArray(imgData, pixelCount, quality) {
         a = pixels[offset + 3];
 
         // If pixel is mostly opaque and not white
-        if (typeof a === 'undefined' || a >= 125) {
+        if (typeof a === "undefined" || a >= 125) {
             if (!(r > 250 && g > 250 && b > 250)) {
                 pixelArray.push([r, g, b]);
             }
@@ -25,75 +25,83 @@ function createPixelArray(imgData, pixelCount, quality) {
 function validateOptions(options) {
     let { colorCount, quality } = options;
 
-    if (typeof colorCount === 'undefined' || !Number.isInteger(colorCount)) {
+    if (typeof colorCount === "undefined" || !Number.isInteger(colorCount)) {
         colorCount = 10;
-    } else if (colorCount === 1 ) {
-        throw new Error('colorCount should be between 2 and 20. To get one color, call getColor() instead of getPalette()');
+    } else if (colorCount === 1) {
+        throw new Error(
+            "colorCount should be between 2 and 20. To get one color, call getColor() instead of getPalette()"
+        );
     } else {
         colorCount = Math.max(colorCount, 2);
         colorCount = Math.min(colorCount, 20);
     }
 
-    if (typeof quality === 'undefined' || !Number.isInteger(quality) || quality < 1) {
+    if (
+        typeof quality === "undefined" ||
+        !Number.isInteger(quality) ||
+        quality < 1
+    ) {
         quality = 10;
     }
 
     return {
         colorCount,
-        quality
-    }
+        quality,
+    };
 }
 
-function loadImg(img) {
+function loadImg(img, type) {
     return new Promise((resolve, reject) => {
-        getPixels(img, function(err, data) {
-            if(err) {
-                reject(err)
+        getPixels(img, type, function (err, data) {
+            if (err) {
+                reject(err);
             } else {
                 resolve(data);
             }
-        })
+        });
     });
 }
 
 function getColor(img, quality) {
     return new Promise((resolve, reject) => {
         getPalette(img, 5, quality)
-            .then(palette => {
+            .then((palette) => {
                 resolve(palette[0]);
             })
-            .catch(err => {
+            .catch((err) => {
                 reject(err);
-            })
+            });
     });
-
 }
 
-function getPalette(img, colorCount = 10, quality = 10) {
+function getPalette(img, colorCount = 10, quality = 10, type = null) {
     const options = validateOptions({
         colorCount,
-        quality
+        quality,
     });
 
     return new Promise((resolve, reject) => {
-        loadImg(img)
-            .then(imgData => {
+        loadImg(img, type)
+            .then((imgData) => {
                 const pixelCount = imgData.shape[0] * imgData.shape[1];
-                const pixelArray = createPixelArray(imgData.data, pixelCount, options.quality);
+                const pixelArray = createPixelArray(
+                    imgData.data,
+                    pixelCount,
+                    options.quality
+                );
 
                 const cmap = quantize(pixelArray, options.colorCount);
-                const palette = cmap? cmap.palette() : null;
+                const palette = cmap ? cmap.palette() : null;
 
                 resolve(palette);
             })
-            .catch(err => {
+            .catch((err) => {
                 reject(err);
-            })
+            });
     });
 }
 
 module.exports = {
     getColor,
-    getPalette
+    getPalette,
 };
-
